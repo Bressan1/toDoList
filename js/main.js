c9e5f757673040c0f56b55ff5ff1cf4a65ce8a9e
Bressan1 <edu.nick7@gmail.com>
2026-02-26 20:49:38 -0300

    create functions delete button, save task, and show who many task u have

--- js/main.js
+++ js/main.js
@@ -17,14 +17,44 @@ inputTarefa.addEventListener('keypress', function(e){
 
 function limparInput(){
     inputTarefa.value = '';
+    inputTarefa.focus();
+};
+
+function criaBotaoAapagar(li){
+    li.innerText += ' ';
+    const botaoApagar = document.createElement('button');
+    botaoApagar.innerText = 'Apagar';
+    botaoApagar.setAttribute('class', 'apagar');
+    li.appendChild(botaoApagar);
 }
 
 function criarTarefa(textoInput){
     const li = criaLi();
     li.innerText = textoInput;
     tarefas.appendChild(li);
+    limparInput();
+    criaBotaoAapagar(li);
 }
 
 btnTarefa.addEventListener('click', function(){
     if(!inputTarefa.value) return;
-})
+});
+
+document.addEventListener('click', function(e){
+    const el = e.target;
+    if(el.classList.contains('apagar')){
+        el.parentElement.remove();
+    }
+});
+
+function salvarTarefas(li){
+    const liTarefas = tarefas.querySelectorAll('li');
+    const listaDeTarefas = [];
+    for(let tarefa of liTarefas){
+        let tarefaTexto = tarefa.innerText;
+        tarefaTexto = tarefaTexto.replace('Apagar', '').trim();
+        listaDeTarefas.push(tarefaTexto);
+    }
+    const tarefasJSON = JSON.stringify(listaDeTarefas);
+    localStorage.setItem('tarefas', tarefasJSON);
+}
